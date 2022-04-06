c80d649f92a7deffa71bebabe77c2f757bee666e
DaNiArI1 <daniel.ariguel@hotmail.com>
2022-04-05 21:29:31 -0300

    primer trabajo entregable

--- ciclo.js
+++ ciclo.js
@@ -1,14 +1,57 @@
+//NOMBRE DE USUARIO
+let nombreUsuario = prompt("Ingresa tu nombre de usuario")
+
+if (nombreUsuario =="") {
+    alert("No pusiste nada crack")
+}
+else {
+    alert("Hola " + nombreUsuario)
+}
+
+//NUMERO DE ORDEN
+
+for (let i = 1; i <= 5; i++) {
+
+    alert(" Turno N "+i+" para "+nombreUsuario)
+    break
+}
+
+//MAYORIA DE EDAD
+
 const mayor = 18
 
-do{
-    edad = parseInt(prompt("Ingresa tu edad"))
+edad = parseInt(prompt(nombreUsuario + " ingresa tu edad"))
     mayorDeEdad = edad >= mayor
     if(mayorDeEdad) {
-        alert("Todo legal, pasa maquina")
+        alert("Todo legal " + nombreUsuario + " pasa maquina")
 
     }else{
         alert("No loco voy a ir preso, raja de aca")
     }
-}while(mayorDeEdad)
 
-alert("Que, sigues aqui malditasea?")
+alert("Te queremos " + nombreUsuario)
+
+//CALCULADORA
+
+function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "+": 
+            return primerNumero + segundoNumero
+            break
+        case "-":
+            return primerNumero - segundoNumero
+            break
+            
+        case "*":
+            return primerNumero * segundoNumero
+            break
+        case "/":
+            return primerNumero / segundoNumero
+            break
+        default:
+            return 0
+            break    
+
+    }
+}
+console.log(calculadora(10, 5, "*"))
